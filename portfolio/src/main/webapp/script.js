f7d23f7c9a3761eaef1e871ac419932d9fa4f45c
Leah <mayoleah25@gmail.com>
2020-05-29 15:43:42 -0400

    remove the active button feature

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -61,14 +61,3 @@ function four() {
     elements[i].style.flex = "25%";
   }
 }
-
-// Add active class to the current button (highlight it)
-var header = document.getElementById("myHeader");
-var btns = header.getElementsByClassName("btn");
-for (var i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function() {
-    var current = document.getElementsByClassName("active");
-    current[0].className = current[0].className.replace(" active", "");
-    this.className += " active";
-  });
-}
